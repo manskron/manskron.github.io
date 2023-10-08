2d1b84602867d9e2cd0e523de3a27446d847ea5e
Måns Bülling <manskron@gmail.com>
2023-10-08 23:05:36 +0200

    Use very cool unicode characters

--- mans.js
+++ mans.js
@@ -3,8 +3,18 @@ import { GameOfLife } from "./game_of_life/index.js";
 // @ts-check
 function setupThemeButton() {
   const btn = document.getElementById("themeToggler");
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    btn.textContent = "☀";
+  } else {
+    btn.textContent = "⏾";
+  }
   if (btn) {
     btn.addEventListener("click", function () {
+      if (btn.textContent == "☀") {
+        btn.textContent = "⏾";
+      } else {
+        btn.textContent = "☀";
+      }
       if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
         document.body.classList.toggle("light-theme");
       } else {
