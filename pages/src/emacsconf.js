745bba470ba4f2efcb1e1d9d17fde1df638706f8
Måns Bülling <manskron@gmail.com>
2023-11-09 22:44:39 +0100

    Remove emacsconf.js

--- pages/src/emacsconf.js
+++
@@ -1,7 +0,0 @@
-let pre = document.getElementById("config");
-
-fetch("https://raw.githubusercontent.com/manskron/modmacs/master/init.el")
-    .then((response) => response.text())
-    .then((text) => {
-        pre.innerHTML = text;
-    });
