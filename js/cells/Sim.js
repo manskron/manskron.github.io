76f5d7862278cbd51807684c84997e2fc7cc0d6e
Måns Bülling <manskron@gmail.com>
2023-11-11 22:50:20 +0100

    Organize

---
+++ js/cells/Sim.js
@@ -0,0 +1,84 @@
+// @ts-check
+export class GameOfLife {
+    constructor(cells) {
+        this.BOARD_COLS = cells;
+        this.BOARD_ROWS = cells;
+        this.board = [];
+        this.initializeBoard();
+    }
+
+    setCellCount(count, randomize = true) {
+        this.BOARD_COLS = count;
+        this.BOARD_ROWS = count;
+        this.initializeBoard(randomize);
+    }
+
+    initializeBoard(randomized = true) {
+        let tempBoard = [];
+        for (let r = 0; r < this.BOARD_COLS; r++) {
+            let rowArr = [];
+            for (let c = 0; c < this.BOARD_ROWS; c++) {
+                rowArr.push(randomized ? Math.floor(Math.random() * 2) : 0);
+            }
+            tempBoard.push(rowArr);
+        }
+
+        this.board = tempBoard;
+    }
+
+    getCell(rowIndex, colIndex) {
+        if (rowIndex < 0) {
+            return 0;
+        }
+        if (rowIndex > this.board.length - 1) {
+            return 0;
+        }
+
+        let cell = this.board[rowIndex][colIndex];
+
+        if (cell === undefined) {
+            return 0;
+        }
+
+        return cell;
+    }
+
+    getNeighbourCount(rowIndex, colIndex) {
+        let count =
+            this.getCell(rowIndex - 1, colIndex - 1) +
+            this.getCell(rowIndex - 1, colIndex) +
+            this.getCell(rowIndex - 1, colIndex + 1) +
+            this.getCell(rowIndex, colIndex - 1) +
+            this.getCell(rowIndex, colIndex + 1) +
+            this.getCell(rowIndex + 1, colIndex - 1) +
+            this.getCell(rowIndex + 1, colIndex) +
+            this.getCell(rowIndex + 1, colIndex + 1);
+
+        return count;
+    }
+
+    getNextCellState(cell, rowIndex, colIndex) {
+        let neighbourCount = this.getNeighbourCount(rowIndex, colIndex);
+        if (cell === 1) {
+            if (neighbourCount < 2 || neighbourCount > 3) {
+                return 0;
+            }
+        } else if (cell == 0 && neighbourCount == 3) {
+            return 1;
+        }
+
+        return cell;
+    }
+
+    getNextBoard() {
+        let newBoard = [];
+        this.board.forEach((row, rowIndex) => {
+            let newRow = [];
+            row.forEach((cell, colIndex) => {
+                newRow.push(this.getNextCellState(cell, rowIndex, colIndex));
+            });
+            newBoard.push(newRow);
+        });
+        this.board = newBoard;
+    }
+}
